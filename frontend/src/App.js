73ad10ffc3998120f89111e229be225ff0a673ea
Mike <mdowd4848@gmail.com>
2024-06-30 22:52:39 -0400

    started more functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
   const [swimmerData, setSwimmerData] = useState([]);
   const [swimmerGuess, setSwimmerGuess] = useState("");
-  const [correctSwimmer, setCorrectSwimmer] = useState("");
+  const [correctSwimmer, setCorrectSwimmer] = useState(null);
+  const [guessList, setGuessList] = useState([]);
+  const [guessFeedbackList, setGuessFeedback] = useState([]);
 
+  // Ref for the input element
+  const inputRef = useRef(null);
 
   //index of the correct swimmer
   const idx_of_answer = 1;
@@ -22,7 +26,9 @@ function App() {
 
       const res_data = await response.json()
       setSwimmerData(res_data.swimmers);
-      setCorrectSwimmer(swimmerData[idx_of_answer]);
+      setCorrectSwimmer(res_data.swimmers[idx_of_answer]);
+
+      console.log(res_data.swimmers[idx_of_answer]);
       
 
       //Check if user has played yet today, if not, set numGuesses
@@ -42,31 +48,124 @@ function App() {
 
   //Function that's called when the user has used up all of their guesses
   function doneForDay() {
-    alert("TODO: done for day");
+    alert("TODO: done for day(out of guesses)");
+  }
+
+  //For testing
+  function restart_game() {
+    localStorage.setItem("numGuesses", "0");
+    setGuessList([]);
+    setGuessFeedback([]);
   }
 
 
   function submitGuess(e) {
     e.preventDefault();
+
+    //The info of the swimmer that was guessed
     const swimmer = swimmerData.find(swimmer => swimmer.Name === swimmerGuess);
 
+    //Lets user know if their guess is valid
+    if(swimmer == undefined) {
+
+      //TODO: add function to deal with this on frontend
+      alert("This swimmer is not a possible answer");
+      return;
+    }
+
+    const guessFeedback = getGuessFeedback(swimmer, correctSwimmer);
+
+    //TODO: handle guess
+    if(swimmer.Name === correctSwimmer.Name) { //correct guess
+      alert("You Win!");
+      restart_game();
+    }
+    else {//incorrect guess
+      setGuessList([...guessList, swimmer]);
+      setGuessFeedback([...guessFeedbackList, guessFeedback]);
+    }
+
+
     const numGuesses = parseInt(localStorage.getItem("numGuesses"));
-    console.log(numGuesses);
+
+    if(numGuesses >= 5) { //Game over
+      doneForDay();
+    }
+
+    //update number of guesses
     localStorage.setItem("numGuesses", `${numGuesses+1}`);
+
+    //Clears the input box after a guess
+    setSwimmerGuess("");
+    inputRef.current.value="";
+
+  }
+
+  function getAge(birthday_string) {
+    let today = new Date();
+
+    let birthday = new Date(birthday_string);
+
+    const diffInMs = today-birthday;
+    const years = diffInMs / (1000*60*60*24*365)
+    const age = Math.floor(years);
+    console.log(birthday_string);
+    console.log(today,birthday);
+
+    return age;
+    
+  }
+
+  //Gets all the feedback on the guess
+  //Whether the age, stroke, college, nationality, etc. is correct or close
+  function getGuessFeedback(swimmerGuess, correctSwimmer) {
+    const guess = swimmerGuess;
+    const correct = correctSwimmer;
+
+    let age, ageColor, stroke, college, isl_team, country = null;
+
+    //set age correctness
+    const guessAge = getAge(guess.Birthday);
+    age=guessAge;
+    console.log("calculated age = ", age);
+    const correctAge = getAge(correct.Birthday);
+    if (guessAge === correctAge) {
+      ageColor = "green";
+    }
+    else if (guessAge < correctAge) {
+      ageColor = "yellow^";
+    }
+    else {
+      ageColor="yellow_";
+    }
+
+
+
+    const feedback = {
+      age: age,
+      ageColor: ageColor
+    }
+
+    return feedback;
   }
 
   return (
     <>
-      <div class="header">
+      <div className="header">
         <h1>SWORDLE</h1>
       </div>
 
-      <div class="game-container">
-        <div class="guess-box">
+      <div className="game-container">
+        <div className="guess-box">
 
-          <form onSubmit={submitGuess} class="guess-form">
+          <form onSubmit={submitGuess} className="guess-form">
           <label>Guess a swimmer:
-          <input list="swimmers" name="swimmers" onChange={(e) => setSwimmerGuess(e.target.value)} /></label>
+          <input ref={inputRef} // Assign ref to input
+                list="swimmers" 
+                name="swimmers" 
+                onChange={(e) => setSwimmerGuess(e.target.value)} />
+
+          </label>
           <datalist id="swimmers">
             {swimmerData.map((swimmer) => (
               <option value={swimmer.Name} key={swimmer._id}/>
@@ -75,6 +174,16 @@ function App() {
             <input type="submit" value="Guess"></input>
           </form>
         </div>
+
+        <ol className="guess-list" id="guess-list">
+            {guessList.map((guess, ind) => (
+              <li key={ind}>
+                {guess.Name}, age = {guessFeedbackList[ind].age}{(guessFeedbackList[ind].ageColor)}
+              </li>
+            ))}
+        </ol>
+
+        <button style={{marginLeft: '50%'}}onClick={restart_game} >restart</button>
       </div>
     </>
   );
